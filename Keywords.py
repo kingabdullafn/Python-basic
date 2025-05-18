4e2b80f9b3e1a19bac616db4cfe12db2a8f4d6a3
kingabdullafn <abdullahharsher@outlook.com>
2025-05-18 13:57:21 +0100

    Add files via upload

---
+++ Keywords.py
@@ -0,0 +1,3 @@
+import keyword
+print ("Hi kids revise these keywords below or you are getting detention")
+print(keyword.kwlist)
---
+++ welcome.py
@@ -0,0 +1,8 @@
+# Python=12
+# print("Hello my age is\n", Python)#My sentecen for my age
+Age=input("How old are you?")
+print("Okay that is cool im 12 Your",Age,"Do you want to be friends?")
+name=input("Hi what is your name?")
+print(f"Nice to meet you {name} welcome to codingal!")
+hobby=input("What is your favourite hobby?")
+print(f"Nice {name} {hobby} sounds cool!")
