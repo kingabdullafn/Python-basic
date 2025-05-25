c951dd4a4ae24ba5b0f9315edf4f28e8491e0985
kingabdullafn <abdullahharsher@outlook.com>
2025-05-25 14:14:01 +0100

    Add files via upload

---
+++ stringoperation.py
@@ -0,0 +1,10 @@
+x=("My name is Abdullah")
+# different types of slicing
+print(x[0:10:1])
+print(x[0:20:3])
+print(x[::-1]) #reverse the string
+
+#lenght and index
+print(len(x))
+print(x[1])
+print(x[7])
---
+++ typecasting.py
@@ -0,0 +1,17 @@
+# Assigning Different Variables
+name="Penguin"
+age= 15
+is_student=True
+weight= 38.5
+print("Data types of Name is", type(name))
+print("Data type of Age is", type (age))
+print("Data type of is_student is", type(is_student))
+print("Data type of weight is", type(weight))
+# Type casting to convert the datatype of variables
+print("/n After Type Casting....")
+age=str(age)
+print("age")
+print("Data type of Age is", type (age))
+weight=int(weight)
+print("weight")
+print("Data type of weight is", type(weight))
