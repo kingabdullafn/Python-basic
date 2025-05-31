d817167c8a339d13f40200a8020c5610acb148af
kingabdullafn <abdullahharsher@outlook.com>
2025-05-31 13:52:48 +0100

    Add files via upload

---
+++ negative.py
@@ -0,0 +1,11 @@
+num=3
+if num> 0:
+    print(num, "is a postive number")
+else:
+    print(num, "is a negative number")
+
+num=-1
+if num> 0:
+    print(num, "is a postive number")
+else:
+    print(num, "is a negative number")
