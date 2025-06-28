9cc322040df1efc6528083d6185690c24fd360cd
kingabdullafn <abdullahharsher@outlook.com>
2025-06-28 10:30:52 +0100

    square root

---
+++ square rooot.py
@@ -0,0 +1,10 @@
+import math
+
+# Yo drop the numberr innit
+num = float(input("What ya sayin drop the number innit fam!"))
+
+# Do the quick maths like shaq
+root = math.sqrt(num)
+
+# Dont underastmate the python powers
+print(f"Boom! like that your square root  {num} is {root}, safe g and if ya need help come to python")
