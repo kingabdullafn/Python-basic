0c14f5f778c3a4c3b72fb46f092c9b05176c31e7
kingabdullafn <abdullahharsher@outlook.com>
2025-06-28 10:20:07 +0100

    hw

---
+++ summertimes.py
@@ -0,0 +1,11 @@
+temperature = int(input("Yo, what's the tempreature sayin'? Drop the digits"))
+
+if temperature >= 50:
+    print("its proper warm out ther like the sahara dessert, shorts and a cap is the defo move here and dont forget to have a ice lolly ☀️☀️☀️☀️")
+
+elif temperature<= 0:
+    print("Fam it's frezzing like an iced chain.Might as well just help billy the snowman to have a life❄️❄️")
+    print("Wooly jacket on, hot cocoa in hand and jut relax and watch a movie")
+
+else: 
+    print("Its one of those days, not hot and not cold just remeber to dress comfy")
