09447f636802e356ea46a305db6fd83d1007edd7
kingabdullafn <abdullahharsher@outlook.com>
2025-06-28 10:53:40 +0100

    letterchecker

---
+++ alphabet.py
@@ -0,0 +1,12 @@
+char = input("Yo blud, drop a single character k:")
+
+# Check length first pattern
+if len(char) != 1:
+    print("thats not i said blud, pattern up and give us on character")
+
+else:
+    if char.isalpha():
+        print(f"cmon g! '{char}' is a proper alphabet certefied by the mandem")
+
+
+
